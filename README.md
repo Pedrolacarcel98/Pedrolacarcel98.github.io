e51b7c3550e02960fcebc34ced08dd4ef028ae42
pedlacjar <115985312+pedlacjar@users.noreply.github.com>
2025-03-31 14:45:27 +0200

    NUEVO REPOSITORIO GIT FISIOCIVIS

---
+++ README.md
@@ -0,0 +1,1 @@
+Este es un proyecto Web creado por Pedro Lacárcel para la empresa Fisiocivs
---
+++ js/buttonHeader.js
@@ -0,0 +1,6 @@
+document.getElementById("menu-toggle").addEventListener("click", function () {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("hidden");
+    menu.classList.toggle("opacity-100");
+    menu.classList.toggle("translate-y-0");
+});
